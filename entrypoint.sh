69cb7676e113e7eefa191db88d9b6d00f72205f8
Konstantin Freidlin <freidlinks@ya.ru>
2024-07-06 21:15:14 +0300

    Fix color for 8bpp

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,7 +36,7 @@ xvfb-run -a --listen-tcp --server-args ":99 -ac -screen 0 320x200x8" ~/DOOM/linu
 sleep 3
 fluxbox -display :99 -screen 0 &
 sleep 3
-x11vnc -display :99.0 -forever -8to24
+x11vnc -display :99.0 -forever -flashcmap -shiftcmap -notruecolor # -nofb -visual TrueColor # -8to24
 #sleep 7
 ## make screenshot
 #xwd -display :99 -root -silent | convert xwd:- png:/output/screenshot.png
